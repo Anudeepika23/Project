c2c4d6e8b803348be4213893399b37edae8da851
Anudeepika23 <121333122+Anudeepika23@users.noreply.github.com>
2023-02-14 21:37:09 +0530

    file uploaded

---
+++ Project/src/Abstraction/Cricle.java
@@ -0,0 +1,8 @@
+package Abstraction;
+
+public  class Cricle extends Shape {
+	public void draw() {
+		System.out.println("drawing crice");
+	}
+	
+}
---
+++ Project/src/Abstraction/Retangle.java
@@ -0,0 +1,8 @@
+package Abstraction;
+
+public class Retangle extends Shape{
+	public void draw(){
+		System.out.println("drawing retangle");
+	}
+
+}
